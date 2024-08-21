2ae940e31b41565a0297a4c6e17e1e603bad80b8
puhi0210 <mihael0210@gmail.com>
2024-08-21 09:35:38 +0200

    scanner

--- ble-scanner.py
+++ ble-scanner.py
@@ -15,8 +15,10 @@ scanner = Scanner().withDelegate(ScanDelegate())
 
 # Scan for devices for 10 seconds
 devices = scanner.scan(10.0)
+print("\nSkeniranje končano.\n")
 
 for dev in devices:
     print("Naprava %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
     for (adtype, desc, value) in dev.getScanData():
         print("  %s = %s" % (desc, value))
+    print("\n")
