5691cbbd5a4e8afe4e359bc4c6048ebf586dbe5f
puhi0210 <mihael0210@gmail.com>
2024-08-29 07:52:40 +0200

    mqtt

--- MQTT-testPub.py
+++ MQTT-testPub.py
@@ -10,14 +10,12 @@ broker_address: str = os.getenv('BROKER_ADDR')
 broker_port: int = int(os.getenv('BROKER_PORT'))
 broker_keepalive: int = int(os.getenv('BROKER_KA'))
 
-print(f"Broker Address: {broker_address}")
 
-'''
 client = paho.Client()
 
 if client.connect(broker_address, broker_port, broker_keepalive) != 0:
     print("Couldn't connect to the mqtt broker")
     sys.exit(1)
-
+'''
 client.publish("puhi0210/test_topic", "Hi, paho mqtt client works fine!", 0)
 client.disconnect()'''
